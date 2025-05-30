f985ccf49a32a25ba1442197414f9f301622dfc0
yenox_laptop <f3lixxx@yandex.ru>
2025-05-30 18:45:28 +0300

    add time

--- main.cpp
+++ main.cpp
@@ -2,10 +2,53 @@
 
 #include <QApplication>
 #include <QFile>
+#include <QTextStream>
+#include <QDateTime>
+#include <QObject>
+#include <QDebug>
+
+QString logFile = "log.ini";
+QDateTime startDateTime;
+
+void logwrite(const QString &message){
+    QFile file(logFile);
+    if(file.open(QIODevice::Append | QIODevice::Text)){
+        QTextStream out(&file);
+        out << message << "\n";
+        file.close();
+    } else {
+        qDebug() << "Не удалось открыть лог файл.";
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString startTime = QDateTime::currentDateTime().toString("hh:mm:ss dd-MM-yyyy");
+
+    startDateTime = QDateTime::currentDateTime();
+    logwrite("Программа запущена в: " + startTime);
+
+    // Реакция на закрытие приложения
+    QObject::connect(&a, &QCoreApplication::aboutToQuit, [&](){
+        QDateTime endDateTime = QDateTime::currentDateTime();
+
+        logwrite("Программа закрыта в: " + endDateTime.toString("hh:mm:ss dd-MM-yyyy"));
+
+        qint64 msecs = startDateTime.msecsTo(endDateTime);
+        int secs = msecs / 1000;
+        int hours = secs / 3600;
+        int minutes = (secs % 3600) / 60;
+        int seconds = secs % 60;
+        QString totalTime = QString("Общее время работы программы: %1 часов, %2 минут, %3 секунд.")
+                                .arg(hours)
+                                .arg(minutes)
+                                .arg(seconds);
+
+        logwrite(totalTime);
+    });
+
     QFile stylesheet_file (":/styles/qss/style.css");
     stylesheet_file.open( QFile :: ReadOnly);
     QString stylesheet = QLatin1String (stylesheet_file.readAll());
